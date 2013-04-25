14f94b2f1bba81f1a96a95c06a5c01bcb0087ade
orangeman <flo@teleportr.org>
2013-04-25 13:03:15 +0200

    tests green

---
+++ src/org/teleportr/DataBaseHelper.java
@@ -0,0 +1,228 @@
+package org.teleportr;
+
+import java.util.HashSet;
+import java.util.Map.Entry;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
+import android.util.Log;
+
+class DataBaseHelper extends SQLiteOpenHelper {
+
+	private SQLiteStatement insertPlace;
+	private SQLiteStatement insertPlaceKey;
+	private SQLiteStatement insertRide;
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("create table places (" +
+				"'_id' integer primary key autoincrement," +
+				" 'geohash' text," + 
+				" 'name' text," +
+				" 'address' text," +
+				" 'lat' integer," +
+				" 'lng' integer" +
+				");");
+		db.execSQL("CREATE UNIQUE INDEX places_idx" +
+				" ON places ('geohash', 'name')" +
+				";");
+		db.execSQL("create table place_keys (" +
+				"'_id' integer primary key autoincrement," +
+				" 'place_id' integer," + 
+				" 'key' text," +
+				" 'value' text" +
+				");");
+		db.execSQL("CREATE UNIQUE INDEX place_keys_idx" +
+				" ON place_keys ('place_id', 'key')" +
+				";");
+		db.execSQL("create table rides (" +
+				"'_id' integer primary key autoincrement," +
+				" 'type' integer," +
+				" 'from_id' integer," +
+				" 'to_id' integer," +
+				" 'dep' integer," +
+				" 'arr' integer," +
+				" 'who' text," +
+				" 'mode' text," +
+				" 'operator' text," +
+				" 'distance' integer," +
+				" 'price' integer," +
+				" 'parent' text," +
+				" 'expire' integer," +
+				" 'key' text" +
+				"); ");
+		db.execSQL("CREATE UNIQUE INDEX rides_idx" +
+				" ON rides ('type', 'from_id', 'to_id'," +
+				" 'dep', 'arr', 'who', 'mode', 'operator');");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersn, int newVersn) {}
+
+
+
+	public DataBaseHelper(Context context) {
+		super(context, "teleportr.db", null, 1);
+		insertPlace = getWritableDatabase().compileStatement(INSERT_PLACE);
+		insertPlaceKey = getWritableDatabase().compileStatement(INSERT_KEY);
+		insertRide = getWritableDatabase().compileStatement(INSERT_RIDE);
+	}
+
+
+	static final String INSERT_PLACE =
+			"INSERT OR IGNORE INTO places" +
+			" ('geohash', 'name', 'address', 'lat', 'lng')" +
+			" VALUES (?, ?, ?, ?, ?);";
+
+	public long insertPlace(ContentValues cv) {
+		insertPlace.bindString(1, cv.getAsString("geohash"));
+		insertPlace.bindString(2, cv.getAsString("name"));
+		insertPlace.bindString(3, cv.getAsString("address"));
+		insertPlace.bindLong(4, cv.getAsLong("lat"));
+		insertPlace.bindLong(5, cv.getAsLong("lng"));
+		long place_id = insertPlace.executeInsert();
+		if (place_id == -1) { // insert has been ignored as place already exists
+			Cursor c = getReadableDatabase().rawQuery("SELECT _id from places" +
+				" WHERE name=?", new String[] { cv.getAsString("name") });
+			c.moveToFirst();
+			place_id = c.getLong(0);
+		}
+		if (cv.size() > 5) {
+			insertPlaceKeys(cv, place_id);
+		}
+		Log.d(RidesProvider.TAG, "+ stored place " + cv.getAsString("geohash"));
+		return place_id;
+	}
+
+
+	static final String INSERT_KEY =
+			"INSERT OR IGNORE INTO place_keys" +
+			" ('place_id', 'key', 'value')" +
+			" VALUES (?, ?, ?);";
+
+	public void insertPlaceKeys(ContentValues cv, long place_id) {
+		insertPlaceKey.bindLong(1, place_id);
+		for (Entry<String, Object> entry : cv.valueSet()) {
+			if (!PLACE_COLUMNS.contains(entry.getKey())) {
+				insertPlaceKey.bindString(2, entry.getKey());
+				insertPlaceKey.bindString(3, (String) entry.getValue());
+				insertPlaceKey.executeInsert();
+				Log.d(RidesProvider.TAG, "+ stored key " + entry);
+			}
+		}
+	}
+
+
+	static final String INSERT_RIDE =
+			"INSERT OR REPLACE INTO rides" +
+			" ('type', 'from_id', 'to_id', 'dep', 'arr', 'who'," +
+			" 'mode', 'operator', 'expire', 'parent', 'key') " +
+			" VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
+	public long insertRide(ContentValues cv) {
+		if (cv.containsKey("type")) 
+			insertRide.bindString(1, cv.getAsString("type"));
+		else
+			insertRide.bindLong(1, 0);
+		if (cv.containsKey("from_id")) 
+			insertRide.bindString(2, cv.getAsString("from_id"));
+		else
+			insertRide.bindString(2, "");
+		if (cv.containsKey("to_id")) 
+			insertRide.bindString(3, cv.getAsString("to_id"));
+		else
+			insertRide.bindString(3, "");
+		if (cv.containsKey("dep")) 
+			insertRide.bindLong(4, cv.getAsLong("dep"));
+		else
+			insertRide.bindLong(4, 0);
+		if (cv.containsKey("arr")) 
+			insertRide.bindLong(5, cv.getAsLong("arr"));
+		else
+			insertRide.bindLong(5, 0);
+		if (cv.containsKey("who")) 
+			insertRide.bindString(6, cv.getAsString("who"));
+		else
+			insertRide.bindString(6, "");
+		if (cv.containsKey("mode")) 
+			insertRide.bindString(7, cv.getAsString("mode"));
+		else
+			insertRide.bindString(7, "");
+		if (cv.containsKey("operator")) 
+			insertRide.bindString(8, cv.getAsString("operator"));
+		else
+			insertRide.bindString(8, "");
+		if (cv.containsKey("expire")) 
+			insertRide.bindLong(9, cv.getAsLong("expire"));
+		else
+			insertRide.bindLong(9, 0);
+		if (cv.containsKey("parent")) 
+			insertRide.bindString(10, cv.getAsString("parent"));
+		else
+			insertRide.bindString(10, "");
+		if (cv.containsKey("key")) 
+			insertRide.bindString(11, cv.getAsString("key"));
+		else
+			insertRide.bindString(11, "");
+		long id = insertRide.executeInsert();
+		Log.d(RidesProvider.TAG, "+ stored ride " + id);
+		return id;
+	}
+
+
+	static final String SELECT_FROM = 
+			"SELECT * FROM 'places'" +
+			" LEFT JOIN (" +
+				"SELECT count(_id) AS count, from_id FROM 'rides'" +
+				" GROUP BY from_id" +
+			") AS history ON _id=history.from_id" +
+			" ORDER BY history.count DESC, name ASC;";
+
+	public Cursor autocompleteFrom() {
+		return getReadableDatabase().rawQuery(SELECT_FROM, null);
+	}
+
+
+	static final String SELECT_TO = 
+			"SELECT * FROM 'places'" +
+			" LEFT JOIN (" +
+				"SELECT count(_id) AS count, to_id FROM 'rides'" +
+				" WHERE from_id=?" +
+				" GROUP BY to_id" +
+			") AS history ON _id=history.to_id" +
+			" WHERE _id<>?" +
+			" ORDER BY history.count DESC, name ASC;";
+
+	public Cursor autocompleteTo(String from) {
+		return getReadableDatabase().rawQuery(SELECT_TO,
+				new String[] {from, from});
+	}
+
+
+	static final String SELECT_RIDES = 
+			"SELECT rides._id," +
+					" \"from\".name, \"from\".address," +
+					" \"to\".name, \"to\".address, dep" +
+			" FROM 'rides'" +
+			" JOIN 'places' AS \"from\" ON from_id=\"from\"._id" +
+			" JOIN 'places' AS \"to\" ON to_id=\"to\"._id" +
+			" WHERE type=" + Ride.OFFER +
+			" ORDER BY dep;";
+
+	public Cursor queryRides() {
+		return getReadableDatabase().rawQuery(SELECT_RIDES, new String[] {});
+	}
+
+	static final HashSet<String> PLACE_COLUMNS = new HashSet<String>(5);
+	static {
+		PLACE_COLUMNS.add("geohash");
+		PLACE_COLUMNS.add("name");
+		PLACE_COLUMNS.add("address");
+		PLACE_COLUMNS.add("lat");
+		PLACE_COLUMNS.add("lng");
+	}
+}
--- src/org/teleportr/RidesProvider.java
+++ src/org/teleportr/RidesProvider.java
@@ -1,252 +1,144 @@
 package org.teleportr;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
-import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.database.sqlite.SQLiteStatement;
 import android.net.Uri;
 import android.util.Log;
 
 public class RidesProvider extends ContentProvider {
-    private static final String TAG = "RideProvider";
-    private DataBaseHelper db;
+
+	static final String TAG = "RideProvider";
+	private DataBaseHelper db;
 	private UriMatcher route;
 
-    class DataBaseHelper extends SQLiteOpenHelper {
-        public DataBaseHelper(Context context) {
-            super(context, "teleportr.db", null, 1);
-        }
+	@Override
+	public boolean onCreate() {
+		db = new DataBaseHelper(getContext());
+		try {
+			setAuthority(getContext().getPackageName());
+		} catch (UnsupportedOperationException e) {
+		}
+		return false;
+	}
 
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            db.execSQL("create table places ("
-                    + "'_id' integer primary key autoincrement, "
-                    + "'key' text, "
-                    + "'value' text, "
-                    + "'lat' integer, "
-                    + "'lng' integer, "
-                    + "'geohash' text"
-                    + ");");
-            db.execSQL("CREATE UNIQUE INDEX places_idx ON places (" +
-                        "key, geohash);");
-            db.execSQL("create table rides ("
-                    + "'_id' integer primary key autoincrement, "
-            		+ "'type' integer, "
-                    + "'from' text, "
-                    + "'to' text, "
-                    + "'dep' integer, "
-                    + "'arr' integer, "
-                    + "'who' text, "
-                    + "'mode' text, "
-                    + "'operator' text, "
-                    + "'distance' integer, "
-                    + "'price' integer, "
-                    + "'parent' text, "
-                    + "'expire' integer, "
-                    + "'key' text);");
-            db.execSQL("CREATE UNIQUE INDEX rides_idx ON rides (" +
-                    "'type', 'from', 'to', 'dep', 'arr', 'who', 'mode', 'operator');");
-        }
+	public void setAuthority(String authority) {
+		route = new UriMatcher(0);
+		route.addURI(authority, "history", HISTORY);
+		route.addURI(authority, "places", PLACES);
+		route.addURI(authority, "places/#", PLACE);
+		route.addURI(authority, "rides", RIDES);
+		route.addURI(authority, "jobs", JOBS);
+	}
 
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersn, int newVersn) {
-        }
-    }
+	@Override
+	public String getType(Uri uri) {
+		return null;
+	}
 
-    @Override
-    public boolean onCreate() {
-        db = new DataBaseHelper(getContext());
-        try {
-        	setAuthority(getContext().getPackageName());
-        } catch (UnsupportedOperationException e) {}
-        return false;
-    }
-    
-    public void setAuthority(String authority) {
-    	route = new UriMatcher(0);
-    	route.addURI(authority, "history", HISTORY);
-    	route.addURI(authority, "places", PLACES);
-        route.addURI(authority, "places/*", PLACE);
-        route.addURI(authority, "rides", RIDES);
-        route.addURI(authority, "jobs", JOBS);
-    }
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		long id = 0;
+		try {
+			db.getWritableDatabase().beginTransaction();
+			switch (route.match(uri)) {
+			case PLACES:
+				id = db.insertPlace(values);
+				break;
+			case RIDES:
+				id = db.insertRide(values);
+				break;
+			}
+			db.getWritableDatabase().setTransactionSuccessful();
+		} catch (Exception e) {
+			Log.e(TAG, "error " + e);
+		} finally {
+			db.getWritableDatabase().endTransaction();
+			getContext().getContentResolver().notifyChange(uri, null);
+		}
+		return ContentUris.withAppendedId(uri, id);
+	}
 
-    @Override
-    public String getType(Uri uri) {
-        return null;
-    }
+	@Override
+	public int bulkInsert(Uri uri, ContentValues[] values) {
+		try {
+			db.getWritableDatabase().beginTransaction();
+			switch (route.match(uri)) {
+			case PLACES:
+				for (int i = 0; i < values.length; i++) {
+					db.insertPlace(values[i]);
+				}
+				break;
+			case RIDES:
+				for (int i = 0; i < values.length; i++) {
+					db.insertRide(values[i]);
+				}
+				break;
+			}
+			db.getWritableDatabase().setTransactionSuccessful();
+		} catch (Exception e) {
+			Log.e(TAG, "error " + e);
+		} finally {
+			db.getWritableDatabase().endTransaction();
+			getContext().getContentResolver().notifyChange(uri, null);
+		}
+		return 1;
+	}
 
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        switch (route.match(uri)) {
-        case PLACES:
-            db.getWritableDatabase().insert("places", null, values);
-            break;
-        case RIDES:
-            db.getWritableDatabase().insert("rides", null, values);
-            break;
-        }
-        getContext().getContentResolver().notifyChange(uri, null);
-        return Uri.withAppendedPath(uri, values.getAsString("key"));
-    }
-    
-    @Override
-    public int bulkInsert(Uri uri, ContentValues[] values) {
-        try {
-            String sql;
-            SQLiteStatement insert;
-            db.getWritableDatabase().beginTransaction();
-            switch (route.match(uri)) {
-            case PLACES:
-                sql = "INSERT OR IGNORE into places "
-                        + "(key, value, lat, lng, geohash) "
-                        + "values (?, ?, ?, ?, ?);";
-                insert = db.getWritableDatabase().compileStatement(sql);
-                for (int i = 0; i < values.length; i++) {
-                    ContentValues cv = values[i];
-                    insert.bindString(1, cv.getAsString("key"));
-                    insert.bindString(2, cv.getAsString("value"));
-                    insert.bindLong(3, cv.getAsLong("lat"));
-                    insert.bindLong(4, cv.getAsLong("lng"));
-                    insert.bindString(5, cv.getAsString("geohash"));
-                    insert.executeInsert();
-                    Log.d(TAG, "stored " + cv.getAsString("geohash"));
-                }
-                break;
-            case RIDES:
-                sql = "INSERT OR IGNORE into rides "
-                        + "('type', 'from', 'to', 'dep', 'arr', 'who'," +
-                        "'mode', 'operator', 'expire', 'parent', 'key') "
-                        + "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";  
-                insert = db.getWritableDatabase().compileStatement(sql);
-                for (int i = 0; i < values.length; i++) {
-                    ContentValues cv = values[i];
-                    if (cv.containsKey("type")) 
-                    	insert.bindString(1, cv.getAsString("type"));
-                    else
-                    	insert.bindLong(1, 0);
-                    if (cv.containsKey("from")) 
-                    	insert.bindString(2, cv.getAsString("from"));
-                    else
-                    	insert.bindString(2, "");
-                    if (cv.containsKey("to")) 
-                    	insert.bindString(3, cv.getAsString("to"));
-                    else
-                    	insert.bindString(3, "");
-                    if (cv.containsKey("dep")) 
-                    	insert.bindLong(4, cv.getAsLong("dep"));
-                    else
-                    	insert.bindLong(4, 0);
-                    if (cv.containsKey("arr")) 
-                    	insert.bindLong(5, cv.getAsLong("arr"));
-                    else
-                    	insert.bindLong(5, 0);
-                    if (cv.containsKey("who")) 
-                    	insert.bindString(6, cv.getAsString("who"));
-                    else
-                    	insert.bindString(6, "");
-                    if (cv.containsKey("mode")) 
-                    	insert.bindString(7, cv.getAsString("mode"));
-                    else
-                    	insert.bindString(7, "");
-                    if (cv.containsKey("operator")) 
-                    	insert.bindString(8, cv.getAsString("operator"));
-                    else
-                    	insert.bindString(8, "");
-                    if (cv.containsKey("expire")) 
-                    	insert.bindLong(9, cv.getAsLong("expire"));
-                    else
-                    	insert.bindLong(9, 0);
-                    if (cv.containsKey("parent")) 
-                    	insert.bindString(10, cv.getAsString("parent"));
-                    else
-                    	insert.bindString(10, "");
-                    if (cv.containsKey("key")) 
-                    	insert.bindString(11, cv.getAsString("key"));
-                    else
-                    	insert.bindString(11, "");
-                    insert.executeInsert();  
-                }
-                break;
-            }
-            db.getWritableDatabase().setTransactionSuccessful();
-        } finally {
-            db.getWritableDatabase().endTransaction();
-            getContext().getContentResolver().notifyChange(uri, null);
-        }
-        return 1;
-    }
+	@Override
+	public Cursor query(Uri uri, String[] proj, String sel, String[] args,
+			String ord) {
+		switch (route.match(uri)) {
+		case HISTORY:
+			return db.getReadableDatabase().query("rides",
+					null, "type=" + Ride.SEARCH, null, null, null, "_id DESC");
+		case PLACE:
+			String key = uri.getQueryParameter("key");
+			if (key != null) {
+				return db.getReadableDatabase().query("place_keys", null,
+						"place_id=" + uri.getLastPathSegment() + 
+						" AND key='" + key + "'", null, null, null, null);
+			} else {
+				return db.getReadableDatabase().query("places", null,
+						"_id=" + uri.getLastPathSegment(),
+						null, null, null, null);
+			}
+		case PLACES:
+			String from_id = uri.getQueryParameter("from_id");
+			if (from_id == null) {
+				return db.autocompleteFrom();
+			} else {
+				return db.autocompleteTo(from_id);
+			}
+		case RIDES:
+			return db.queryRides();
+		}
+		return null;
+	}
 
-    @Override
-    public Cursor query(Uri uri, String[] proj, String sel, String[] args, String ord) {
-        switch (route.match(uri)) {
-        case HISTORY:
-            return db.getReadableDatabase().query("rides", null,
-            		"type=" + Ride.SEARCH, null, null, null, "_id DESC");
-        case PLACE:
-        	String where = "geohash='"+uri.getLastPathSegment()+"'";
-        	String key = uri.getQueryParameter("key");
-        	if (key != null)
-        		where += " AND key='"+key+"'";
-        	return db.getReadableDatabase().query("places",
-        			null, where, null, null, null, null);
-        case PLACES:
-        	String from = uri.getQueryParameter("from");
-        	if (from == null) {
-        		return db.getReadableDatabase().rawQuery(
-        				"SELECT R._id, name.value, address.value, R.\"from\" FROM 'rides' AS R " +
-        						"LEFT JOIN 'places' AS name ON R.\"from\" = name.geohash AND name.key = 'name' " +
-        						"LEFT JOIN 'places' AS address ON R.\"from\" = address.geohash AND address.key = 'address' " +
-        						"GROUP BY R.\"from\" ORDER BY count(R.\"from\") DESC;", null);
-        	} else {
-        		return db.getReadableDatabase().rawQuery(
-        				"SELECT R._id, name.value, address.value, R.\"to\" FROM 'rides' AS R " +
-        						"LEFT JOIN 'places' AS name ON R.\"to\" = name.geohash AND name.key = 'name' " +
-        						"LEFT JOIN 'places' AS address ON R.\"to\" = address.geohash AND address.key = 'address' " +
-        						"WHERE \"from\" IS '"+from+"' " +
-        						"GROUP BY R.\"to\" ORDER BY count(R.\"to\") DESC;", null);
-        	}
-        case RIDES:
-        	return db.getReadableDatabase().rawQuery( // this is totally FAIL
-    				"SELECT R._id, from_name.value, from_address.value, to_name.value, to_address.value, dep FROM 'rides' AS R " +
-    				"LEFT JOIN 'places' AS from_name ON R.\"from\" = from_name.geohash AND from_name.key = 'name' " +
-    				"LEFT JOIN 'places' AS from_address ON R.\"from\" = from_address.geohash AND from_address.key = 'address' " +
-    				"LEFT JOIN 'places' AS to_name ON R.\"to\" = to_name.geohash AND to_name.key = 'name' " +
-    				"LEFT JOIN 'places' AS to_address ON R.\"to\" = to_address.geohash AND to_address.key = 'address' " +
-    				"WHERE R.type=" + Ride.OFFER + " ORDER BY dep", null);
-        }
-        return null;
-    }
+	@Override
+	public int update(Uri uri, ContentValues values, String sel, String[] args) {
+		switch (route.match(uri)) {
+		case JOBS:
+			db.getWritableDatabase().update("rides", values, sel, null);
+			break;
+		}
+		return 0;
+	}
 
-    @Override
-    public int update(Uri uri, ContentValues values, String sel, String[] args) {
-        switch (route.match(uri)) {
-        case JOBS:
-            db.getWritableDatabase().update("rides", values, sel, null);
-            break;
-        }
-        return 0;
-    }
+	@Override
+	public int delete(Uri arg0, String arg1, String[] arg2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    @Override
-    public int delete(Uri arg0, String arg1, String[] arg2) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-    
-    
-    
-    
-    private static final int HISTORY = 0;
-    private static final int PLACES = 1;
-    private static final int PLACE = 2;
-    private static final int RIDES = 3;
-    private static final int JOBS = 4;
+	private static final int HISTORY = 0;
+	private static final int PLACES = 1;
+	private static final int PLACE = 2;
+	private static final int RIDES = 3;
+	private static final int JOBS = 4;
+	
 
-    
-    
 }
