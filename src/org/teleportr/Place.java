b681806018a6e58d0a216ea856bf49971e0958b0
orangeman <flo@teleportr.org>
2013-04-25 13:01:21 +0200

    update public API

--- src/org/teleportr/Place.java
+++ src/org/teleportr/Place.java
@@ -1,95 +1,103 @@
 package org.teleportr;
 
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
-
 import ch.hsr.geohash.GeoHash;
 
-
 public class Place {
-	
-	public static ArrayList<ContentValues> batch = new ArrayList<ContentValues>();
-	public static Context context;
 
-	public Place set(String key, String value) {
-        ContentValues values = new ContentValues();
-        values.put("geohash", geohash);
-        values.put("lat", lat);
-        values.put("lng", lng);
-        values.put("value", value);
-        values.put("key", key);
-        batch.add(values);
-        return this;
+	public long id;
+	protected ContentValues cv;
+
+	public Place(long id) {
+		this.id = id;
 	}
 
-	public String get(String key) {
-		Cursor values = context.getContentResolver().query(Uri.parse(
-				"content://" + context.getPackageName() +
-				"/places/"+geohash+"?key="+key),
-                null, null, null, null);
-        String value = null;
-        if (values.getCount() != 0) {
-            values.moveToFirst();
-            value = values.getString(2);
-        }
-        values.close();
-        return value;
+	public Place() {
+		cv = new ContentValues();
 	}
 
-    public int lat;
-    public int lng;
-    public String geohash;
-    
-    public Place(String geohash) {
-        this.geohash = geohash;
-        try {
-            GeoHash gh = GeoHash.fromGeohashString(geohash);
-            this.lat = (int) (Math.round(gh.getPoint().getLatitude() * 1E6));
-            this.lng = (int) (Math.round(gh.getPoint().getLongitude() * 1E6));
-        } catch (NullPointerException e) {
-        }
-    }
+	public Place(String geohash) {
+		cv = new ContentValues();
+		cv.put("geohash", geohash);
+		try {
+			GeoHash gh = GeoHash.fromGeohashString(geohash);
+			cv.put("lat", (int) (Math.round(gh.getPoint().getLatitude() * 1E6)));
+			cv.put("lng",
+					(int) (Math.round(gh.getPoint().getLongitude() * 1E6)));
+		} catch (NullPointerException e) {
+			System.out.println("not a geohash: " + geohash);
+		}
+	}
 
-    public Place(int lat, int lng) {
-        geohash = GeoHash.withBitPrecision(
-                ((double) lat) / 1E6, ((double) lng) / 1E6, 55).toBase32();
-        this.lat = lat;
-        this.lng = lng;
-    }
-    
-    public Place(double lat, double lng) {
-        this(GeoHash.withBitPrecision(lat, lng, 55).toBase32());
-    }
-    
-    public double getLat() {
-    	return ((double) lat) / 1E6;
-    }
-    
-    public double getLng() {
-    	return ((double) lng) / 1E6;
-    }
-    
-    @Override
-    public String toString() {
-        return geohash;
-    }
+	public Place(int lat, int lng) {
+		cv = new ContentValues();
+		cv.put("geohash",
+				GeoHash.withBitPrecision(((double) lat) / 1E6,
+						((double) lng) / 1E6, 55).toBase32());
+		cv.put("lat", lat);
+		cv.put("lng", lng);
+	}
+
+	public Place(double lat, double lng) {
+		this(GeoHash.withBitPrecision(lat, lng, 55).toBase32());
+	}
+
+	public double getLat() {
+		return ((double) cv.getAsInteger("lat")) / 1E6;
+	}
+
+	public double getLng() {
+		return ((double) cv.getAsInteger("lng")) / 1E6;
+	}
+
+	@Override
+	public String toString() {
+		return cv.getAsString("geohash");
+	}
 
 	public Place name(String name) {
-		set("name", name);
+		cv.put("name", name);
 		return this;
 	}
-
 
 	public Place address(String address) {
-		set("address", address);
+		cv.put("address", address);
 		return this;
 	}
 
+	public Place set(String key, String value) {
+		cv.put(key, value);
+		return this;
+	}
+
+	public Uri store(Context ctx) {
+		if (!cv.containsKey("address"))
+			cv.put("address", cv.getAsString("name"));
+		if (!cv.containsKey("geohash"))
+			cv.put("geohash", "");
+		if (!cv.containsKey("lat"))
+			cv.put("lat", 0);
+		if (!cv.containsKey("lng"))
+			cv.put("lng", 0);
+		Uri uri = ctx.getContentResolver().insert(
+				Uri.parse("content://" + ctx.getPackageName() + "/places"), cv);
+		id = Long.parseLong(uri.getLastPathSegment());
+		return uri;
+	}
+
+	public String get(String key, Context ctx) {
+		Cursor values = ctx.getContentResolver().query(
+				Uri.parse("content://" + ctx.getPackageName() + "/places/" + id
+						+ "?key=" + key), null, null, null, null);
+		String value = null;
+		if (values.getCount() != 0) {
+			values.moveToFirst();
+			value = values.getString(3);
+		}
+		values.close();
+		return value;
+	}
 }
--- src/org/teleportr/Ride.java
+++ src/org/teleportr/Ride.java
@@ -1,85 +1,76 @@
 package org.teleportr;
 
-import java.util.ArrayList;
 import java.util.Date;
 
-import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.net.Uri;
 
-
-
 public class Ride {
-	
+
 	public static final int SEARCH = 42;
 	public static final int OFFER = 47;
 
-	ContentValues values;
-
-	public static ArrayList<ContentValues> batch = new ArrayList<ContentValues>();
-	
-	public static void saveAll(Context context) {
-		
-		context.getContentResolver().bulkInsert(
-				Uri.parse("content://" + context.getPackageName() + "/places"),
-				(ContentValues[]) Place.batch.toArray(new ContentValues[Place.batch.size()]));
-		Place.batch.clear();
-        
-		context.getContentResolver().bulkInsert(
-				Uri.parse("content://" + context.getPackageName() + "/rides"),
-        		(ContentValues[]) batch.toArray(new ContentValues[batch.size()]));
-        batch.clear();
-	}
+	ContentValues cv;
 
 	public Ride() {
-		values = new ContentValues();
-		batch.add(values);
-	}	
-	
-	public static Ride store() {
-		return new Ride();
+		cv = new ContentValues();
 	}
-	
-	
+
+	public Uri store(Context ctx) {
+		return ctx.getContentResolver().insert(
+				Uri.parse("content://" + ctx.getPackageName() + "/rides"), cv);
+	}
+
 	public Ride from(Place from) {
-		values.put("from", from.geohash);
+		return from(from.id);
+	}
+
+	public Ride from(Uri from) {
+		return from(Integer.parseInt(from.getLastPathSegment()));
+	}
+
+	public Ride from(long from_id) {
+		cv.put("from_id", from_id);
 		return this;
 	}
-	
-	public Ride from(String from) {
-		values.put("from", from);
-		return this;
-	}
-	
+
 	public Ride to(Place to) {
-		values.put("to", to.geohash);
+		return to(to.id);
+	}
+
+	public Ride to(Uri to) {
+		return to(Integer.parseInt(to.getLastPathSegment()));
+	}
+
+	public Ride to(long to_id) {
+		cv.put("to_id", to_id);
 		return this;
 	}
-	
+
 	public Ride to(String to) {
-		values.put("to", to);
+		cv.put("to", to);
 		return this;
 	}
-	
+
 	public Ride dep(Date dep) {
-		values.put("dep", dep.getTime());
+		cv.put("dep", dep.getTime());
 		return this;
 	}
-	
+
 	public Ride arr(Date arr) {
-		values.put("arr", arr.getTime());
+		cv.put("arr", arr.getTime());
 		return this;
 	}
-	
+
 	public Ride who(String who) {
-		values.put("who", who);
+		cv.put("who", who);
 		return this;
 	}
 
 	public Ride type(int type) {
-		values.put("type", type);
+		cv.put("type", type);
 		return this;
 	}
-	
+
 }
